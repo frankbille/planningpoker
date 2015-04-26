aaf39e636d49712e660add5b46cbb79dea3c7b3b
Frank Bille <github@frankbille.dk>
2015-04-26 13:39:55 +0200

    Try to see if the travis build/deployment works now

--- travis-ci-deploy.sh
+++ travis-ci-deploy.sh
@@ -2,6 +2,7 @@
 branch=$(git symbolic-ref --short -q HEAD)
 echo "${GAE_CLIENT_KEY_JSON_FILE}" > gcloudkey.json
 google-cloud-sdk/bin/gcloud auth activate-service-account \
+    "${GAE_CLIENT_ACCOUNT}"
     --key-file gcloudkey.json
 google-cloud-sdk/bin/gcloud \
     --project "${GAE_PROJECT_ID}" \
