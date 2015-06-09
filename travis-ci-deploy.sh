9293b4e258b2da42dc5cbcfcc7751f40f4000a5e
Frank Bille <github@frankbille.dk>
2015-06-09 21:57:50 +0200

    Run grunt build correctly
    Also make the bash script correct.

--- travis-ci-deploy.sh
+++ travis-ci-deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 node_modules/firebase-tools/bin/firebase login --email ${FIREBASE_EMAIL} --password ${FIREBASE_PASSWORD}
 firebase=planningpokerapp
-if [ "${TRAVIS_BRANCH}" -eq "develop" ]; then
+if [ "${TRAVIS_BRANCH}" = "develop" ]; then
     firebase=planningpokerappdev
 fi
 node_modules/firebase-tools/bin/firebase deploy --firebase ${firebase}
